1ee5e977c971910422899351f4ea5b2314f3a9da
minuk0506 <leeminuk9238@daum.net>
2022-07-28 17:45:19 +0900

    0728

---
+++ Hello/app/src/main/java/com/minuk/hello/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package com.minuk.hello.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
